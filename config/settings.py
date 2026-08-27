605438b9e87113f1cfaa7228cb91ecc00a939b50
Shalu Jain <ruhirj14@gmail.com>
2026-08-27 14:05:48 +0530

    Initial_commit

---
+++ config/settings.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MONGODB_URI=os.getenv("MONGODB_URI")
+MONGODB_DB=os.getenv("MONGODB_DB","fast_api_db")
+
+
---
+++ database/connection.py
@@ -0,0 +1,6 @@
+from mongoengine import connect
+from config.settings import MONGODB_DB, MONGODB_URI
+
+
+def connect_database():
+    connect(db= MONGODB_DB, host=MONGODB_URI)
